cf5d5dd1a1785b32e0447654fe6136688ce22d8b
Milena Tomova <kokolina1888@gmail.com>
2015-12-28 20:39:37 +0200

    hw-11-2014

---
+++ js-su-intro/hw_11_2014/task02/numbers.js
@@ -0,0 +1,4 @@
+for (var i = 1; i <= 10; i++) {
+	console.log(i);
+}
+//http://stackoverflow.com/questions/15206849/how-to-execute-a-hello-world-javascript-file-in-node-js
--- js-su-intro/hw_3_2015/task02/radius.js
+++ js-su-intro/hw_3_2015/task02/radius.js
@@ -2,7 +2,7 @@
  function myJsFunction(){
     var radius = document.getElementById('input1').value;
     var preArea = Math.pow(radius, 2);
-    var area = preArea * 3.14;
+    var area = preArea * Math.PI;
 
     document.getElementById("result").innerHTML = "r = " + radius + " area = " + area;
 
