216f593f675d54357b7d9d6bc7b1ba35eadd6e0c
Milena Tomova <kokolina1888@gmail.com>
2016-04-02 13:32:58 +0300

    exams

--- js-su-intro/ui&dom/exam_05_04_2015/03-GoshkoTheRabbit/pr03.js
+++ js-su-intro/ui&dom/exam_05_04_2015/03-GoshkoTheRabbit/pr03.js
@@ -7,9 +7,9 @@
 //a wall he doesn’t eat from the same cell again.
 function solve(args){
 	var dir = args[0].split(', '),
-	matrix = args.splice(1), mLen, 
-	dLen, startR = 0, startC = 0, newR, newC, flag = 0, sample, veg, noVeg = 0, car = 0,
-	cab = 0, letu = 0, tur = 0, cells = [], 
+	matrix = args.splice(1), mLen, rowLength,
+	dLen, startR = 0, startC = 0, newR, newC, flag = 0, sample, veg, noVeg = 0, car = 0, cellStr = '',
+	cab = 0, letu = 0, tur = 0, cells = [], coord = [], coordFlag = 0,
 	pattern, wallHits = 0;
 	mLen = matrix.length;
 
@@ -24,8 +24,8 @@ function solve(args){
 	dLen = dir.length;
 	newR = startR;
 	newC = startC;
+	rowLength = matrix[0].length;
 	for(var j = 0; j < dLen; j+=1){
-
 		switch(dir[j]) {
 			case 'up':
 			newR = startR-1;
@@ -39,19 +39,15 @@ function solve(args){
 			case 'down':
 			newR = startR+1;					
 			break;			
-		} 
-		if(newR < 0){ newR = startR; wallHits+=1; flag = 1;}
-		if(newC < 0){ newC = startC; wallHits+=1; flag = 1;}
+		} //switch
+		if(newR < 0 || newR > rowLength-1){ newR = startR; wallHits+=1; flag = 1;}
+		if(newC < 0 || newC > mLen-1){ newC = startC; wallHits+=1; flag = 1;}
 		startR = newR;
-		startC = newC;
-		match = matrix[startR][startC];
-		
-		
-//carrots – {!}, cabbage – {*}, lettuce – {&}, turnip – {#}
-		//
+		startC = newC;		
+			match = matrix[startR][startC];
 		if (flag === 0) {
 			sample = pattern.exec(match); 
-
+			if(sample !== null){
 			veg = sample[0]; 
 			switch(veg) {
 				case '{!}':
@@ -71,29 +67,45 @@ function solve(args){
 				noVeg = 1;
 				break;					
 			}
-			
-		}
+			var coordLen = coord.length;
+
+for (var s = 0; s < coordLen; s+=1){
+	var newCoord = startR+'-'+startC;
+	if(newCoord === coord[s]){
+		coordFlag = 1;
+	}
+}
+if (coordFlag === 0) {
+	coord.push(newCoord);
+	cells.push(match);
+}
+		}			
+		
 		//console.log(flag);
 		flag = 0;
-		var celLen = cells.length;
-		cellFlag = 0;
-		for(var c = 0; c< celLen; c+=1){
-			if(match === cells[c]){
-				cellFlag = 1;
-			}
-		}
-		if (cellFlag === 0) {
-			
-			cells.push(match);}
-		}
+
+		//matrix[startR][startC]
+
+		// var celLen = cells.length;
+		// cellFlag = 0;
+		// for(var c = 0; c< celLen; c+=1){
+		// 	if(match === cells[c]){
+		// 		cellFlag = 1;
+		// 	}
+		// }
+		// if (cellFlag === 0) {			
+		// 	cells.push(match);
+		// }
+
+	}
+}
 // 	var str = "Visit Microsoft!";
 // var res = str.replace("Microsoft", "W3Schools"); 
 
 str = '{"&": ' + letu+',"*": '+cab+',"#": '+tur+',"!": '+car+',"wall hits": '+wallHits+'}';
 console.log(str);
 celLen = cells.length;
-if(celLen > 0){
-	var cellStr = '';
+if(celLen > 0){	
 	for(var b = 0; b < celLen; b+=1){
 		cells[b] = cells[b].replace(pattern, '@');
 		cellStr += cells[b];
@@ -104,13 +116,16 @@ if(celLen > 0){
 	console.log(cellStr);
 }
 else if (celLen === 0){
-	console.log('no');
+	cellStr = 'no';
+	console.log(cellStr);
 }
 
 }
 test = [
-'up, right, left, down',
-'as{!}xnk'
-
+'right, right, down, left, left, down',
+'qwe{!}kjs, a{*}s{&}da, mxz',
+'q{#}w{!}ekjs, asda, xnc{*}a',
+'qwe{#}kjs, as{#}da, xnca',
+'qwekjs, as{#}{#}da, xnca',
 ]
 solve(test);
